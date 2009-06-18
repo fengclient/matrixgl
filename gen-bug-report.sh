2eed935975cc5b02d42ab54a3970710fb503727a
Vincent Launchbury <vincent@doublecreations.com>
2009-06-18 19:52:10 -0400

    Our autoconf tests now obey the silent options

--- gen-bug-report.sh
+++ gen-bug-report.sh
@@ -19,6 +19,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
 
+# Note: We assume the user has dialog, but if they don't,
+#  it still generates a useful report.
+
 if test $EUID -eq 0; then
    echo "Error: Don't run this as root"
    exit
