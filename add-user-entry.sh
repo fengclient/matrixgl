84eb3e25a034e59e45e573cc23ab49892ee8da67
Vincent Launchbury <vincent@doublecreations.com>
2009-10-20 01:16:39 -0400

    Added script to add entry to ~/.xscreensaver

---
+++ add-user-entry.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# Script to add matrixgl entry to ~/.xscreensaver file.
+# Copyright (C) Vincent Launchbury 2009 
+# Written By: Vincent Launchbury <vincent@doublecreations.com>
+# -------------------------------------------
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+
+# On most distros, when xscreensaver-demo is run, it
+# searchs the hack/conf directories for new hacks and 
+# adds them to the list. It doesn't work on some distros
+# (like openbsd), so this script adds it for us. 
+
+
+# Make sure ~/.xscreensaver exists
+if test ! -e ~/.xscreensaver; then
+   echo "Error: File '~/.xscreensaver' does not exist"
+   echo "Are you sure you have xscreensaver installed?"
+   exit 1
+fi
+
+# If it's empty, user needs to run xscreensaver-demo to generate
+if test ! -s ~/.xscreensaver; then
+   echo "Error: '~/.xscreensaver' is empty"
+   echo "Please run xscreensaver-demo and run this script again"
+   exit 1
+fi
+
+# Exit if the entry is already there
+if test `grep matrixgl ~/.xscreensaver|wc -l` -gt 0; then
+   echo "Error: Matrixgl already in list of screensavers"
+   exit 0
+fi
+
+# Line of last entry
+# FIXME: This won't work on very old versions of sh.
+#  and using `cmd` won't work because of the pipe.
+LENTRY=$(sed -n /\\\\n/= ~/.xscreensaver | sed $\!d)
+
+# Add entry after last entry
+sed "$LENTRY a GL:\t\tmatrixgl -root -C green\t         \\\\n\\\\" ~/.xscreensaver > .tmp-$$-xs
+
+# Overwrite users file
+mv -f .tmp-$$-xs ~/.xscreensaver
+rm -f .tmp-$$-xs
+
+# Print success
+echo "Matrixgl successfully added to xscreensaver list."
+echo "Use xscreensaver-demo to select matrixgl and edit it's settings"
