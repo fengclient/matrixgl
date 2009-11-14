1e6c76eae86d74517904c5c76e3ccad64f3ff515
Stephane Sudre <s.sudre@free.fr>
2009-11-14 16:38:45 -0500

    Added Mac OSX Port (source) by Stephane Sudre

---
+++ macsrc/MGLSaverView.h
@@ -0,0 +1,48 @@
+#import <AppKit/AppKit.h>
+#import <ScreenSaver/ScreenSaver.h>
+#import <OpenGL/gl.h>
+#import <OpenGL/glu.h>
+
+#include "matrix.h"
+
+@interface MGLSaverView : ScreenSaverView
+{
+	IBOutlet id IBconfigureSheet_;
+	
+	IBOutlet id IBversion_;
+	
+	IBOutlet id IBshowCredits_;
+	
+	IBOutlet id IBshowImages_;
+	
+	IBOutlet id IBmainScreen_;
+    
+	IBOutlet id IBcolor_;
+	
+	// Data
+	  
+	NSOpenGLView *_view;
+    
+    NSInteger mainScreenOnly_;
+    
+    BOOL isConfiguring_;
+	
+    BOOL preview_;
+	
+    BOOL mainScreen_;
+    
+    
+    
+    MGLSettings * settings_;
+}
+
+- (void) doScroll:(NSNumber *) inNumber;
+
+- (IBAction) closeSheet:(id) sender;
+
+- (IBAction) switchImages:(id) sender;
+
+- (void) readDefaults:(ScreenSaverDefaults *) inDefaults;
+- (void) writeDefaults;
+
+@end
